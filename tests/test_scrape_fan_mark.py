496441ffbef80da4bb0cc77c2575ea4a56659f1a
えてみなる <47494433+eteeeeeerminal@users.noreply.github.com>
2021-08-06 17:36:13 +0900

    test: add test_greedy_emoji

--- tests/test_scrape_fan_mark.py
+++ tests/test_scrape_fan_mark.py
@@ -1,5 +1,6 @@
 import pytest
 from src.scrape_fan_mark import (
+    greedy_emoji,
     make_emoji_page_url
 )
 
@@ -7,3 +8,7 @@ def test_make_emoji_page_url():
     assert "https://v-data.info/e/F09FA6B4" == make_emoji_page_url("🦴")
     assert "https://v-data.info/e/F09F98BA" == make_emoji_page_url("😺")
     assert "https://v-data.info/e/E29ABD" == make_emoji_page_url("⚽")
+
+def test_greedy_emoji():
+    assert "🇦🇫" == greedy_emoji("🇦🇫👽")
+    assert "😺" == greedy_emoji("😺🧑")
